8dca285abfe9a5428152895d5a640e12c70007d4
MaartenDirickx <Not available>
2024-03-30 21:11:19 +0100

    Add comment about the program's goal

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,39 @@
+/**
+ * I'll be quite verbose in my comments and follow Jerry Coffin's advice:
+ * "The code is only there to explain the comments to the machine"
+ * 
+ * GOAL
+ * ----
+ * This microcontroller code is written to make a big robot move. It does this 
+ * by getting 
+ * signals from a typical hobby RC transceiver, and translating those to 
+ * motor movement.  As this is potentialy dangerous, several fail safes are in 
+ * place. Additionaly, there is are some status lights.
+ * 
+ * INPUT
+ * -----
+ * Most RC remote controls have self-centering levers. One forward/reverse, and 
+ * another for left/right. This mimics a basic joystick and can be seen as such.
+ * The receiver yields a 50hz Pulse Width Modulation signal. Of the 20ms pulse,
+ * the HIGH signal only last between 1 and 2 milliseconds. The default pulse is
+ * 1.5ms. One millisecond means full reverse (or Left), two milliseconds is full
+ *  forward (or Right).
+ * 
+ * OUTPUT
+ * ------
+ * To be specified 
+ * #TODO
+ * 
+ * FAILSAFE
+ * --------
+ * The most important fail-safe is the transceiver. If the control PWM signal 
+ * disappears, all movement must halt immediately. This is also tied to a 
+ * status LED. Furthermore, there is an Emergency Stop button on the robot that 
+ * cuts power if something does go wrong. This physical button is NOT part of 
+ * the code.
+ * 
+*/
+
 #include <Arduino.h>
 
 int LED = 13;						// sets LED to pin 13
